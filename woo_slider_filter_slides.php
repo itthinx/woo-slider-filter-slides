0b5432bd1e6c31636661e3bbe29b60ce4f6e2f51
itthinx <itthinx@gmail.com>
2014-11-29 16:04:20 +0100

    Update woo_slider_filter_slides.php

--- woo_slider_filter_slides.php
+++ woo_slider_filter_slides.php
@@ -1,6 +1,23 @@
+<?php
+/**
+ * Plugin Name: WooSlider Filter Slides
+ * Plugin URI: http://www.itthinx.com/plugins/groups-restrict-categories
+ * Description: Enables filters for slides from WooSlider.
+ * Author: itthinx
+ * Author URI: http://www.itthinx.com
+ * Version: 1.0.0
+ */
+
+/**
+ * Plugin main class.
+ */
 class Woo_Slider_Filter_Slides {
-  public static function init() {
-    //
-  }
+	
+	/**
+	 * Setup.
+	 */
+	public static function init() {
+		add_filter( '', '' );
+	}
 }
 add_action( 'init', array( 'Woo_Slider_Filter_Slides', 'init' ) );
